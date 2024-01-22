e8bb008eae6009e78dcdf3a22301890564bd2fa2
Radiant-F <radiantfadilah0@gmail.com>
2024-01-22 15:41:23 +0700

    initial commit

---
+++ src/screens/SignIn/SignIn.tsx
@@ -0,0 +1,12 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function SignIn(): React.JSX.Element {
+  return (
+    <View>
+      <Text>SignIn</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
