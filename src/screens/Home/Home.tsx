36b681fb5e6503961f71009fafa65292dcc8d671
Radiant-F <radiantfadilah0@gmail.com>
2024-01-23 15:57:18 +0700

    navigation props

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,12 +1,15 @@
-import {StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, View} from 'react-native';
 import React from 'react';
+import {HomeProps} from '../../routes';
 
-export default function Home(): React.JSX.Element {
+export default function Home({navigation}: HomeProps): React.JSX.Element {
   return (
     <View>
       <Text>Home</Text>
+      <Button
+        title="navigate to somewhere"
+        onPress={() => navigation.navigate('SignIn')}
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({});
