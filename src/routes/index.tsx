65ca5e2cf28deec2daee20dd8b01de88b99eabdc
Radiant-F <radiantfadilah0@gmail.com>
2024-01-25 16:01:39 +0700

    import root stack *& remove type

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,22 +2,14 @@ import React from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {Home, SignIn, SignUp} from '../screens';
 import type {NativeStackScreenProps} from '@react-navigation/native-stack';
-
-type RootStackParamList = {
-  Home: undefined;
-  SignIn: undefined;
-};
-
-export type HomeProps = NativeStackScreenProps<RootStackParamList, 'Home'>;
-export type SignInProps = NativeStackScreenProps<RootStackParamList, 'SignIn'>;
-
-const RootStack = createNativeStackNavigator<RootStackParamList>();
+import RootStack from './navigationConfig';
 
 export default function StackNavigator(): React.JSX.Element {
   return (
     <RootStack.Navigator>
       <RootStack.Screen name="Home" component={Home} />
       <RootStack.Screen name="SignIn" component={SignIn} />
+      <RootStack.Screen name="SignUp" component={SignUp} />
     </RootStack.Navigator>
   );
 }
