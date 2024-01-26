e3fa2789268e1d1c6bb0e45673513cb01e47fac2
Radiant-F <radiantfadilah0@gmail.com>
2024-01-26 15:51:14 +0700

    provide redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
 import {StyleSheet, Text, View} from 'react-native';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
+import store from './redux/store';
+import {Provider} from 'react-redux';
 
 export default function App(): React.JSX.Element {
   return (
-    <NavigationContainer>
-      <View>
-        <Text>App</Text>
-      </View>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <View>
+          <Text>App</Text>
+        </View>
+      </NavigationContainer>
+    </Provider>
   );
 }
 
