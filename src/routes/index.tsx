f9ce239de75120196c5d17c3de327d6f62b6fca5
Radiant-F <radiantfadilah0@gmail.com>
2024-01-27 19:49:46 +0700

    update import & stack screen

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
-import {Home, SignIn, SignUp} from '../screens';
-import type {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {Home, SignIn, SignUp, Splash} from '../screens';
 import RootStack from './navigationConfig';
 
 export default function StackNavigator(): React.JSX.Element {
@@ -10,6 +8,7 @@ export default function StackNavigator(): React.JSX.Element {
       <RootStack.Screen name="Home" component={Home} />
       <RootStack.Screen name="SignIn" component={SignIn} />
       <RootStack.Screen name="SignUp" component={SignUp} />
+      <RootStack.Screen name="Splash" component={Splash} />
     </RootStack.Navigator>
   );
 }
