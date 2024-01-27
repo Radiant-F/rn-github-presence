b9d2602edce9afa81ef1b8c86c2c18ea878aaa87
Radiant-F <radiantfadilah0@gmail.com>
2024-01-27 19:49:35 +0700

    update import

--- src/screens/index.js
+++ src/screens/index.js
@@ -1,5 +1,6 @@
 import Home from './Home/Home';
 import SignIn from './SignIn/SignIn';
 import SignUp from './SignUp/SignUp';
+import Splash from './SplashScreen/Splash';
 
-export {Home, SignIn, SignUp};
+export {Home, SignIn, SignUp, Splash};
