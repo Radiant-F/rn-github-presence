14f109e928c43ccfd9cef321a93440f21d5df196
Radiant-F <radiantfadilah0@gmail.com>
2024-02-14 23:04:47 +0700

    apply api slice

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,10 +1,14 @@
 import {configureStore} from '@reduxjs/toolkit';
 import authReducer from '../features/auth/services/authSlice';
+import {apiSlice} from './api/apiSlice';
 
 const store = configureStore({
   reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
   },
+  middleware: defaultMiddleware =>
+    defaultMiddleware().concat(apiSlice.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
