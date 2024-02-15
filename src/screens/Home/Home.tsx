8d4816ea8d9397cbc36608649ffd7160734a9c7d
Radiant-F <radiantfadilah0@gmail.com>
2024-02-15 15:17:56 +0700

    update styling

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,11 +1,18 @@
-import {Button, Text, View} from 'react-native';
+import {ActivityIndicator, Button, Text, View} from 'react-native';
 import React from 'react';
 import {HomeProps} from '../../routes/navigationConfig';
+import {useUserQuery} from '../../features/auth/services/authApiSlice';
 
 export default function Home({navigation}: HomeProps): React.JSX.Element {
+  const {isFetching} = useUserQuery(null);
   return (
     <View>
       <Text>Home</Text>
+      <ActivityIndicator
+        animating={isFetching}
+        size={'large'}
+        color={'black'}
+      />
       <Button
         title="navigate to somewhere"
         onPress={() => navigation.navigate('SignIn')}
