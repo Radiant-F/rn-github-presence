bdbe382a0e2fe172a6aab5a652456575016163f2
Radiant-F <radiantfadilah0@gmail.com>
2024-03-19 15:53:50 +0700

    what

--- src/screens/__test__/LibDemo.test.tsx
+++ src/screens/__test__/LibDemo.test.tsx
@@ -6,17 +6,42 @@ import {
   useCameraPermission,
   useCodeScanner,
 } from 'react-native-vision-camera';
+import axios from 'axios';
 
 export default function LibDemo() {
-  const {hasPermission, requestPermission} = useCameraPermission();
+  const {hasPermission} = useCameraPermission();
   const device = useCameraDevice('back');
 
-  const [modal, setModal] = useState(true);
+  const [active, setActive] = useState(true);
+
+  const body = {
+    status: 'Hadir',
+    latitude: 0,
+    longitude: 0,
+    desc: '',
+  };
+  const header = {
+    headers: {Authorization: `Bearer ${token}`, Accept: 'application/json'},
+  };
+  const token =
+    'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvZGV2LnBvbmRva2RpZ2l0YWwucG9uZG9rcXUuaWRcL2FwaVwvbG9naW4iLCJpYXQiOjE3MTA4MzEzNDEsImV4cCI6MTcxMzQyMzM0MSwibmJmIjoxNzEwODMxMzQxLCJqdGkiOiIwVGtVMmpUT0F5ZWpnNnB0Iiwic3ViIjo2NDIsInBydiI6Ijg3ZTBhZjFlZjlmZDE1ODEyZmRlYzk3MTUzYTE0ZTBiMDQ3NTQ2YWEifQ.yTn01x_zumeQKEest1Su44vfke9AxvUSilIIJjMDdBE';
 
   const codeScanner = useCodeScanner({
     codeTypes: ['qr', 'ean-13'],
-    onCodeScanned: codes => {
-      console.log(`Scanned ${codes.length} codes!`);
+    onCodeScanned: async codes => {
+      setActive(false);
+      console.log(codes[0].value);
+      try {
+        const endpoint = codes[0].value == 'Hadir' ? 'in' : 'out';
+        const {data} = await axios.post(
+          `https://dev.pondokdigital.pondokqu.id/api/presence-${endpoint}`,
+          body,
+          header,
+        );
+        console.log('SUCCESS:', data);
+      } catch (error) {
+        console.log('ERROR:', error);
+      }
     },
   });
 
@@ -28,10 +53,11 @@ export default function LibDemo() {
         <Camera
           style={StyleSheet.absoluteFill}
           device={device}
-          isActive={true}
+          isActive={active}
           codeScanner={codeScanner}
         />
       )}
+      <Button title="activate camera" onPress={() => setActive(true)} />
       <Modal visible={!hasPermission}>
         <View
           style={{
