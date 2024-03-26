57a86f500c5e2dc1397a2d9ff3abfae795d90289
Radiant-F <radiantfadilah0@gmail.com>
2024-03-26 20:30:24 +0700

    rename extention

--- src/screens/index.js
+++
@@ -1,6 +0,0 @@
-import Home from './Home/Home';
-import SignIn from './SignIn/SignIn';
-import SignUp from './SignUp/SignUp';
-import Splash from './SplashScreen/Splash';
-
-export {Home, SignIn, SignUp, Splash};
---
+++ src/screens/index.ts
@@ -0,0 +1,8 @@
+import Home from './Home/Home';
+import SignIn from './SignIn/SignIn';
+import SignUp from './SignUp/SignUp';
+import Splash from './SplashScreen/Splash';
+import VisionCamera from './RNVisionCamera/VisionCamera';
+import LibDemo from './__test__/LibDemo.test';
+
+export {Home, SignIn, SignUp, Splash, VisionCamera, LibDemo};
