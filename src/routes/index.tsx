941140add85a969171aad7977d431f3ac0162eb5
Radiant-F <radiantfadilah0@gmail.com>
2024-04-04 23:28:30 +0700

    update root stack

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
-import {Home, SignIn, SignUp, Splash} from '../screens';
+import {Home, VisionCamera, SignIn, SignUp, Splash, LibDemo} from '../screens';
 import RootStack from './navigationConfig';
 
 export default function StackNavigator(): React.JSX.Element {
   return (
-    <RootStack.Navigator>
+    <RootStack.Navigator initialRouteName="LibDemo">
       <RootStack.Screen name="Home" component={Home} />
       <RootStack.Screen name="SignIn" component={SignIn} />
       <RootStack.Screen name="SignUp" component={SignUp} />
-      <RootStack.Screen name="Splash" component={Splash} />
+      <RootStack.Screen
+        name="Splash"
+        options={{headerTitle: 'Home'}}
+        component={Splash}
+      />
+      <RootStack.Screen name="VisionCamera" component={VisionCamera} />
+      <RootStack.Screen name="LibDemo" component={LibDemo} />
     </RootStack.Navigator>
   );
 }
